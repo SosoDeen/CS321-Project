2b3b57d8377a747c36e3eb844eabeee87760cb63
sdevine4 <starlingrin4576@gmail.com>
2023-10-10 17:05:19 -0400

    Created Review Class
    Added variable and class methods to the new Review Class

---
+++ Review.java
@@ -0,0 +1,33 @@
+class Review{
+    String MODULEID = "Review";
+    DocumentRequestForm form;
+    /**
+    * This method will compare the form ID to the information of the A# to check
+    *if the form is correct. If the form is not correct then the form is edited.
+    *
+    * @return true if it was able to be corrected or correct, false if otherwise.
+    **/
+    public boolean review(int formID){
+        
+        return false;
+    }
+    /**
+     * This method changes the desired type to be the new updated data
+     * @param type
+     * @param form
+     * @return
+     */
+    private DocumentRequestForm editData(String type, DocumentRequestForm form){    
+        return this.form;
+    }
+    /**
+     * saves data to database. 
+     * @return String the message from the database
+     */
+    public String saveData(){
+        return null;
+    }
+
+
+
+}
