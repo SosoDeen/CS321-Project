beb021d30120a73630e0a0541aec5d1ba64e73a9
sdevine4 <starlingrin4576@gmail.com>
2023-10-10 16:18:43 -0400

    Created a tester document
    barebones that has some inports

---
+++ Tester.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.*;
+import org.junit.Test;
+class Tester{
+    @Test
+    public void reviewTest(){
+        Review review = new Review();
+        assertEquals()
+
+
+        
+    }
+}
+
+
+/*public static void main(String args){
+    
+    
+    return 0;
+
+}*/
