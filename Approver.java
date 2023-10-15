4d840d689aa32ddb6af30e44f64c36956cc3ec36
TheWOB <williamobrien2001@gmail.com>
2023-10-15 12:45:28 -0400

    Just a little work on Approver

--- Approver.java
+++ Approver.java
@@ -1,4 +1,30 @@
 public class Approver {
     String MODULEID = "Approver"; //needs to be constant
+    String askerID;
     DocumentRequestForm form;
+
+    /** 
+     * This method is meant to check if the data in the form
+     * is valid. 
+     * 
+     * Note that this method leads into acceptAndEmail if data is valid 
+     * and rejectAndReturn if data is invalid
+     * 
+     * @return true if valid and false otherwise.
+     **/
+    public boolean dataTest(){
+        return false;
+    }
+
+    public boolean loadData(){
+        return false;
+    }
+
+    public boolean acceptAndEmail(){
+        return false;
+    }
+
+    public boolean rejectAndReturn(){
+        return false;
+    }
 }
