850a741f25c986d87a8c22ab653d2c87d9b42326
sdevine4 <starlingrin4576@gmail.com>
2023-10-16 16:01:34 -0400

    added getter methods

--- DocumentRequestForm.java
+++ DocumentRequestForm.java
@@ -16,6 +16,27 @@ public class DocumentRequestForm {
         this.status = status;
         // soso wuz here too
     }
+    public String getName(){
+        return null;
+    }
+    public String getDob(){
+        return null;
+    }
+    public String getAddress(){
+        return null;
+    }
+    public int getANum(){
+        return 0;
+    }
+    public int getFormID(){
+        return 0;
+    }
+    public String getDocName(){
+        return null;
+    }
+    public String getStatus(){
+        return null;
+    }
     public DocumentRequestForm createForm(String name, String dob, String address,int formID, int aNum, String docName, String status){
         return null;
         //return new DocumentRequestForm(name, dob, address, formID, aNum, docName, status);
