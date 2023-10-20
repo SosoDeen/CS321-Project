009341ff9e607b879c9b3b5cd657d27f97c3a597
sdevine4 <starlingrin4576@gmail.com>
2023-10-20 19:47:10 -0400

    Added to Project manager

--- ProjectManager.java
+++ ProjectManager.java
@@ -2,13 +2,29 @@ import java.util.Queue;
 
 public class ProjectManager {
     public Queue<Integer> formIDQueue;
-
+    int size;
+    /**
+     * Constructor for Project manager
+     */
+    public ProjectManager(){
+        size = 0;
+    }
+    /**
+     * Finds and removes the next task for the role.
+     * @param moduleID the type of role
+     * @return int the formID, returns 0 if none
+     */
     public int nextTask(String moduleID){
         //return formIDQueue.poll();
         return 0;
     }
-
-    public String addTask(String sendeeModule, int formID){
+    /**
+     * Adds a task back into the queue.
+     * @param moduleID the id of the next job for the form.
+     * @param formID the formID
+     * @return String success on succes and an error message on failure
+     */
+    public String addTask(String moduleID, int formID){
         //formIDQueue.add(formID);
         return "error Message";
     }
