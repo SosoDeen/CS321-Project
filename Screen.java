54e4d3944af7602ba2171ebf217bc62fb4036dcd
sdevine4 <starlingrin4576@gmail.com>
2023-11-01 21:12:20 -0400

    edited screen

--- Screen.java
+++ Screen.java
@@ -1,8 +1,9 @@
 import javafx.application.Application;
 import javafx.event.*;
-import javafx.scene.Scene;
+import javafx.scene.*;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Screen extends Application{
@@ -11,16 +12,24 @@ public class Screen extends Application{
         launch(args);
     }
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Form Document");
-        button = new Button();
-        button.setText("Next");
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Form Document");
+        Group root = new Group();
+        // button = new Button();
+        // button.setText("Next");
+        //StackPane layout = new StackPane();
+        //layout.getChildren().add(button);
 
-        Scene scene = new Scene(layout, 300, 200);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        Scene scene = new Scene(root, 950, 950);
+
+        Text text = new Text();
+        text.setText("Name: ");
+        text.setX(20);
+        text.setY(20);
+
+        root.getChildren().add(text);
+        stage.setScene(scene);
+        stage.show();
     }
     
 }
