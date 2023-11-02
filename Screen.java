f60ebcbad120f0a4d84efd580cc1e3a1a2be96fa
sdevine4 <starlingrin4576@gmail.com>
2023-11-01 20:37:32 -0400

    Started screen using a tutorial

--- Screen.java
+++ Screen.java
@@ -1,3 +1,26 @@
-public class Screen{
+import javafx.application.Application;
+import javafx.event.*;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Screen extends Application{
+    Button button;
+    public static void main(String[] args){
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Form Document");
+        button = new Button();
+        button.setText("Next");
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300, 200);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
     
 }
