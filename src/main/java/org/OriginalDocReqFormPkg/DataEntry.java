03c28cdd4deb94bd68f3d6b06ea70b3f4a668273
SosoDeen <somayazaheer@gmail.com>
2023-11-28 15:28:34 -0500

    Working Maven Project with Working Testers
    OMG DID WE FINALLY DO IT?!
    Note: Made a small changes to star screen to minmize warnings.

--- src/main/java/org/OriginalDocReqFormPkg/DataEntry.java
+++ src/main/java/org/OriginalDocReqFormPkg/DataEntry.java
@@ -1,10 +1,8 @@
 package org.OriginalDocReqFormPkg;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.util.Date;
 import java.util.Locale;
 
 public class DataEntry {
